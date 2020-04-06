dc2b8617da45121cae3e1c288702d25f78aea111
plumest <chayathon.k@ku.th>
2020-04-06 11:02:26 +0700

    Make it Snow ❄️ ☃️

--- 01 - Falling Snow/start.js
+++ 01 - Falling Snow/start.js
@@ -19,22 +19,46 @@
     return [...Array(numberOfSnowBalls)].map(() => {
       return {
         x: random(0, canvas.width),
-        y: random(0, canvas.height)
+        y: random(0, canvas.height),
+        opacity: random(0.5, 1),
+        radius: random(2, 4),
+        speedX: random(-5, 5),
+        speedY: random(1, 3)
       }
     });
   }
 
   function drawSnowBall(canvasContext, snowBall) {
     canvasContext.beginPath();
-    canvasContext.arc(snowBall.x, snowBall.y, 4, 0, Math.PI * 2);
-    canvasContext.fillStyle = `rgba(255, 255, 255, 0.9)`;
+    canvasContext.arc(snowBall.x, snowBall.y, snowBall.radius, 0, Math.PI * 2);
+    canvasContext.fillStyle = `rgba(255, 255, 255, ${snowBall.opacity})`;
     canvasContext.fill();
+  }
+
+  function moveSnowball(canvas, snowBall) {
+    snowBall.x += snowBall.speedX;
+    snowBall.y += snowBall.speedY;
+
+    if (snowBall.x > canvas.width) {
+      snowBall.x = 0;
+    } else if (snowBall.x < 0) {
+      snowBall.x = canvas.width;
+    }
+
+    if (snowBall.y > canvas.height) {
+      snowBall.y = 0;
+    }
   }
 
   function run() {
     const { canvas, canvasContext, numberOfSnowBalls } = setUp();
     const snowBalls = createSnowBalls(canvas, numberOfSnowBalls);
-    snowBalls.forEach(snowBall => drawSnowBall(canvasContext, snowBall));
+
+    setInterval(() => {
+      canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+      snowBalls.forEach(snowBall => drawSnowBall(canvasContext, snowBall));
+      snowBalls.forEach(snowBall => moveSnowball(canvas, snowBall));
+    }, 50);
   }
 
   run();
